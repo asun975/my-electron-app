66e1a850f2e064a606966b6d44ec3811b3fb732c
asun975 <sunas@sheridancollege.ca>
2025-02-27 19:36:31 -0500

    added ipc modules

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 //console.log('Hello from Electron')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
   createWindow()
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
---
+++ renderer.js
@@ -0,0 +1,10 @@
+// Send message from renderer to main process via ipc modules
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // prints out 'pong'
+}
+  
+func()
+
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
